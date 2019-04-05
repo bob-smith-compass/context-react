b4ec2e3415a7b8e76f144895cfd2281e02513aaa
bob-smith-compass <david.shams@gmail.com>
2019-04-05 13:29:59 -0500

    <Segment inverted> and         <ButtonStock />

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import ColorButtons from './components/color-buttons/ColorButtons';
+import ButtonStock from './components/button-stock/ButtonStock';
+import { Segment } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Segment inverted>
         <h3>Context</h3>
+        <ButtonStock />
         <ColorButtons />
-      </div>
+      </Segment>
     );
   }
 }
