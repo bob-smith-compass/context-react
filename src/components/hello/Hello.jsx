f673202ff8ab247e7397240fe8065054ef0363f4
bob-smith-compass <david.shams@gmail.com>
2019-04-07 15:51:52 -0500

    this.handleClick = this.handleClick.bind(this);

--- src/components/hello/Hello.jsx
+++ src/components/hello/Hello.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Segment, Button } from 'semantic-ui-react';
 
 export default class Hello extends Component {
   constructor(props) {
@@ -6,12 +7,20 @@ export default class Hello extends Component {
     this.state = {
       name: "John Doe"
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+      console.log(this.props); // props undefined unless you bind
   }
   render() {
     return (
       <div>
         Props: <div>{this.props.name}</div>
         State: <div>{this.state.name}</div>
+        <Segment>
+            <Button color='red' onClick={this.handleClick}>Red</Button>
+        </Segment>
+
       </div>
     )
   }
