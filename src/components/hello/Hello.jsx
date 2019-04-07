57879365c4c0d7d4705aa8f4a75a9b3cdfd12a5c
bob-smith-compass <david.shams@gmail.com>
2019-04-07 15:45:21 -0500

    props vs state

--- src/components/hello/Hello.jsx
+++ src/components/hello/Hello.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe"
+    }
+  }
   render() {
     return (
       <div>
-        <div>{this.props.name}</div>
+        Props: <div>{this.props.name}</div>
+        State: <div>{this.state.name}</div>
       </div>
     )
   }
