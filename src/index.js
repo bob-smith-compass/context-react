61b9d008c6ca7f5012b019c449b84d97a7acda0a
bob-smith-compass <david.shams@gmail.com>
2019-04-07 13:08:03 -0500

    Redux applyMiddleware createStore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import * as Redux from 'redux';
+import { applyMiddleware, createStore} from 'redux';
+
+
+/**
+ * Create Redux Store
+ */
+let defaultAppState = {name: 'David Shams'};
+function mreducer(state=defaultAppState, action) {
+    console.log("state ", state);
+    if( action.type === 'ADD_TITLE') {
+        return {
+            ...state, title: 'New Title'
+        }
+    } 
+    return state;
+}
+let store = createStore(mreducer, defaultAppState);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
